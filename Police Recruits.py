46c07245e5812e6feeb910d67334554707c51ff8
jocelynlooi <2400090105@stu.pku.edu.cn>
2024-10-05 20:01:07 +0800

    5/10

--- Police Recruits.py
+++ Police Recruits.py
@@ -5,10 +5,12 @@ officers=0
 for i in a:
     if i== -1 and officers==0:
         cnt+=1
-        if i>0:
-            officers+=i
-            continue
-        officers-=1
-print (cnt)
+        continue
+    if i>0:
+        officers+=i
+        continue
+    officers-=1
+
+print(cnt)
 
 
